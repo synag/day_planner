40e0130a51d353ed876ea13d8a5a222d14c5cce4
Carl Benbow <cbenbow@synergisticagility.com>
2021-03-23 09:19:06 -0400

    fixed js and now the text area color changes based on past, presnt future

--- 02-Homework/Develop/planner.js
+++ 02-Homework/Develop/planner.js
@@ -1,62 +1,55 @@
 //Description: Build a Work Day Sheduler that records and stores your weekly plans. 
 //Features
-
-
-
 document.getElementById("currentDay").innerHTML = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
- 
-var hourBlocks = document.querySelectorAll(".hour");
+var hourBlocks = document.querySelectorAll(".time");
 
-
-for ( var i =0; i<hourBlocks.length; i++){
+// Use Textarea for selector and hard code it to 
+//Make updates
+for (var i=0; i<hourBlocks.length; i++){
   var currentTime = moment().format("H");
-  var currentBlockTime = hourBlocks[i].innerText;
-  var currentBlock24 = moment(currentBlockTime, "h A").format("H")
-  console.log(currentBlock24);
-  
-  
-  if (currentTime > currentBlock24){//Past
+  var currentBlockTime = hourBlocks[i].getAttribute("id");
+  console.log(currentBlockTime);
 
-    //find value and set id to past
-    var textArea = document.getElementsByClassName(currentBlock24)
-    console.log(textArea)
-    // var hourTextArea1 = document.getElementsByTagName("textarea")[i];
-    // var att = document.createAttribute("class");  
-    // // console.log(hoursBlock.children[0])
-    //    // Get the first <h1> element in the document
-    // var att = document.createAttribute("id");       // Create a "class" attribute
-    // att.value = [10-i];                           // Set the value of the class attribute
-    // hourTextArea1.setAttributeNode(att);                          // Add the class attribute to <h1>
-  
+   if (currentTime > currentBlockTime){//Past
+
+    //find value and set id to past  -NOT FINISHED
+    var textAreaColor = document.getElementById(currentBlockTime)
+    var newID = document.createAttribute("id");
+    newID.value = "past";                          
+    textAreaColor.setAttributeNode(newID);                         
+                            
     
-  } else if (currentTime < currentBlock24){//Future
-    var textArea = document.getElementsByClassName(currentBlock24)
-    console.log(textArea)
-    
+  } else if (currentTime < currentBlockTime){//Future
+    var textAreaColor = document.getElementById(currentBlockTime)
+    var newID = document.createAttribute("id");
+    newID.value = "future";                          
+    textAreaColor.setAttributeNode(newID);   
+
   } else { //Present
-    
-    var textArea = document.getElementsByClassName(currentBlock24)
-    console.log(textArea)
+   
+    var textAreaColor = document.getElementById(currentBlockTime)
+    var newID = document.createAttribute("id");
+    newID.value = "present";                          
+    textAreaColor.setAttributeNode(newID);    
+     
     
   }
-
-//   setAttribute("class", ) //add.class 
-  // convert currentBlockTime to 24 hr format
-  
-  // compare currentBlockTime to the currentTime from moment
-  
-  // add past, present, future depending on the comparisons
-  
   
 }
 
+// save to local storage (NOT FINISHED)
+var buttonEl = document.getElementsByClassName("saveBtn")
+var textEl =document.querySelectorAll("textarea")
+//Loops through all the button click
+for (var i = 0; i < buttonEl.length; i++) {
+    
+  buttonEl[i].addEventListener("click", function(event) {
+    var saveText = event.value.previousElementSibling.innerText;
+    localStorage.setItem("planner-text",saveText);
+    console.log(saveText)
+ });
 
 
-
-//Save plans in local storage (Store Item)
-//localStorage.setItem("plans","value);
-
-
-
-//Display plans on html after refresh (Get Item)
-//Color changes based on past, present, and future (Set Attribute)(Need to compate time )
+ }
+// //Display plans on html after refresh (Get Item)
+// //Color changes based on past, present, and future (Set Attribute)(Need to compate time )
